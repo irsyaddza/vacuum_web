cc1f8782af4ff78e345125037c6209fbbf482cf9
Irsyad Zainul Abidin <irsyadza04@gmail.com>
2025-12-01 21:56:52 +0700

    route daya hisap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DayahisapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,4 +8,11 @@ Route::get('/', function () {
 });
 Route::get('about', function () {
     return view('about');
+});
+
+// Routes untuk Daya Hisap
+Route::prefix('dayahisap')->group(function () {
+    Route::post('store', [DayahisapController::class, 'store'])->name('dayahisap.store');
+    Route::get('show', [DayahisapController::class, 'show'])->name('dayahisap.show');
+    Route::get('latest', [DayahisapController::class, 'latest'])->name('dayahisap.latest');
 });
